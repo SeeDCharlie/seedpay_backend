0ac0d271a65b01fcf96af580c01f8da77b8eeaf2
unknown <hellocarloscastillo@gmail.com>
2021-09-03 03:06:25 -0500

    add main system microservice

--- main_system/manage.py
+++ main_system/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventas_microservice.settings.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_system.settings.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
