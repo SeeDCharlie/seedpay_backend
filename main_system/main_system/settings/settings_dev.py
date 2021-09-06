4646e33b220bbb0e2ae4da93fe331b754f771ceb
unknown <hellocarloscastillo@gmail.com>
2021-09-06 00:53:52 -0500

    add cors origin

--- main_system/main_system/settings/settings_dev.py
+++ main_system/main_system/settings/settings_dev.py
@@ -67,6 +67,8 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,7 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'main_system.urls'
@@ -95,7 +97,8 @@ TEMPLATES = [
     },
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 """REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
