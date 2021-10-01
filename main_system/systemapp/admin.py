15fb057f69f4e9723cbf31c2cbdec59cb45d8356
seed <seed@debian-BULLSEYE-live-builder-AMD64>
2021-09-30 22:59:29 -0500

    register model on admin site

--- main_system/systemapp/admin.py
+++ main_system/systemapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(categoria_negocio)
+admin.site.register(categoria_productos)
+admin.site.register(categoria_servicios)
+admin.site.register(departamento)
+admin.site.register(tipo_usuario)
+admin.site.register(tipo_identificacion)
