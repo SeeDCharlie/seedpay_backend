8a39208991978b5189ad4bf4c072eb8f81aecff5
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-10-18 20:24:46 -0500

    prometheus and grafana

--- main_system/main_system/settings/settings_dev.py
+++ main_system/main_system/settings/settings_dev.py
@@ -118,7 +118,7 @@ PASSWORD_HASHERS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django_prometheus.db.backends.postgresql',
         'NAME': config['DB_NAME'] ,
         'USER': config['DB_USER'],
         'PASSWORD': config['DB_PASS'],
