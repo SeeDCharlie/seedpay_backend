58cb3293786e125ee66c36493593bca0851414a7
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-11-20 21:36:55 -0500

    refactor passenger

--- main_system/main_system/asgi.py
+++ main_system/main_system/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_system.settings.settings.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_system.settings.settings_prod.py')
 
 application = get_asgi_application()
