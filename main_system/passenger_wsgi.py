93c11add2e634a0dc9c8666edb13b50ef1624dab
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-11-20 20:44:06 -0500

    refactor passenger

--- main_system/passenger_wsgi.py
+++ main_system/passenger_wsgi.py
@@ -1,17 +1,17 @@
 
 import sys, os
 
-INTERP = "/home/admseed/envSeeDPay/bin/python3.9"
+INTERP = "/home/seedch/envSeeDPay/bin/python3.9"
 #INTERP is present twice so that the new python interpreter 
 #knows the actual executable path 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 cwd = os.getcwd()
 sys.path.append(cwd)
-sys.path.append(cwd + '/seedpay_backend/mainsystem')  #You must add your project here
+sys.path.append(cwd + '/mainsystem')  #You must add your project here
 
-sys.path.insert(0,'/home/admseed/envSeeDPay/bin')
-sys.path.insert(0,'/home/admseed/envSeeDPay/lib/python3.9/site-packages')
+sys.path.insert(0,'/home/seedch/envSeeDPay/bin')
+sys.path.insert(0,'/home/seedch/envSeeDPay/lib/python3.9/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "main_system.settings.settings_prod"
 from django.core.wsgi import get_wsgi_application
