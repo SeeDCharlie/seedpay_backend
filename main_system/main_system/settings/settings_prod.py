f6cab698f513eaa212051f4df4cdb5852ebf498f
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-11-20 21:04:47 -0500

    refactor passenger

--- main_system/main_system/settings/settings_prod.py
+++ main_system/main_system/settings/settings_prod.py
@@ -114,7 +114,7 @@ PASSWORD_HASHERS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django_prometheus.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME') ,
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
