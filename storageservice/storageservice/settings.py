6d77d56853f5173647ac6a962f984b922afe8b67
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-11-22 13:22:59 -0500

    add configuration prod storageservice

--- storageservice/storageservice/settings.py
+++ storageservice/storageservice/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['seedpaystorage.seedcharlie.co','seedpay.seedcharlie.co','seedpaymain.seedcharlie.co']
+ALLOWED_HOSTS = ['seedpaystorage.seedcharlie.co']
 
 
 #credenciales y datos aws
