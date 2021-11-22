d1eab6bfed44b8c6c18fa143e7c0d84406dfda94
carlos.castillo1 <carlos.castillo@pragma.com.co>
2021-11-22 13:25:23 -0500

    add configuration prod storageservice

---
+++ storageservice/passenger_wsgi.py
@@ -0,0 +1,17 @@
+import sys, os
+
+INTERP = "/home/seedch/envSeeDPay/bin/python3.9"
+#INTERP is present twice so that the new python interpreter 
+#knows the actual executable path 
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/storageservice')  #You must add your project here
+
+sys.path.insert(0,'/home/seedch/envSeeDPay/bin')
+sys.path.insert(0,'/home/seedch/envSeeDPay/lib/python3.9/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "storageservice.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
